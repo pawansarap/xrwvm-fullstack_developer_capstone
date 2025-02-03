b22450c1b7482ad605cbe3c83dca238d24696b52
Pawan <pawansarap@gmail.com>
2025-02-03 21:47:48 +0530

    setup the site

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'frontend/static')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,6 +74,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'frontend/static')
 ]
 
 WSGI_APPLICATION = 'djangoproj.wsgi.application'
@@ -134,5 +140,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
 
